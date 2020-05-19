cf2b1b905c4e424957609b1c70d9f8f26c38151b
anymarvel <314599558@qq.com>
2020-05-19 14:50:11 +0800

    基于X509TrustManager添加对所有https网站的兼容,兼容爬虫内容

--- app/src/main/java/com/mp/android/apps/monke/monkeybook/base/MBaseModelImpl.java
+++ app/src/main/java/com/mp/android/apps/monke/monkeybook/base/MBaseModelImpl.java
@@ -4,6 +4,12 @@ import com.mp.android.apps.monke.basemvplib.EncodoConverter;
 
 import java.util.concurrent.TimeUnit;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -16,7 +22,44 @@ public class MBaseModelImpl {
             .readTimeout(10, TimeUnit.SECONDS)
             .addInterceptor(new ProxyInterceptor());
 
+    TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+        @Override
+        public void checkClientTrusted(
+                java.security.cert.X509Certificate[] x509Certificates,
+                String s) throws java.security.cert.CertificateException {
+        }
+
+        @Override
+        public void checkServerTrusted(
+                java.security.cert.X509Certificate[] x509Certificates,
+                String s) throws java.security.cert.CertificateException {
+        }
+
+        @Override
+        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+            return new java.security.cert.X509Certificate[]{};
+        }
+    }};
+
     protected Retrofit getRetrofitObject(String url) {
+        clientBuilder.hostnameVerifier(new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        });
+        //创建管理器
+        try {
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+
+            //为OkHttpClient设置sslSocketFactory
+            clientBuilder.sslSocketFactory(sslContext.getSocketFactory());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return new Retrofit.Builder().baseUrl(url)
                 //增加返回值为字符串的支持(以实体类返回)
                 .addConverterFactory(ScalarsConverterFactory.create())
@@ -35,5 +78,6 @@ public class MBaseModelImpl {
                 .client(clientBuilder.build())
                 .build();
     }
+
 
 }

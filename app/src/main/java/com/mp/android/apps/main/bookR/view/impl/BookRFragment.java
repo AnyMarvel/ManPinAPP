3499fc34889712556e89f334df1719d72562a304
anymarvel <314599558@qq.com>
2020-07-01 10:25:26 +0800

    增加Viewpage滑动指示器,增加滑动字体变化提醒

--- app/src/main/java/com/mp/android/apps/main/bookR/view/impl/BookRFragment.java
+++ app/src/main/java/com/mp/android/apps/main/bookR/view/impl/BookRFragment.java
@@ -7,36 +7,43 @@ import android.widget.TextView;
 
 import androidx.viewpager.widget.ViewPager;
 
+import com.lwj.widget.viewpagerindicator.ViewPagerIndicator;
 import com.mp.android.apps.R;
 import com.mp.android.apps.main.bookR.adapter.MybookViewPagerAdapter;
 import com.mp.android.apps.main.bookR.presenter.IBookRFragmentPresenter;
 import com.mp.android.apps.main.bookR.presenter.impl.BookRFragmentPresenterImpl;
 import com.mp.android.apps.main.bookR.view.IBookRFragmentView;
 import com.mp.android.apps.monke.basemvplib.impl.BaseFragment;
+import com.mp.android.apps.monke.monkeybook.widget.refreshview.BaseRefreshListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class BookRFragment extends BaseFragment<IBookRFragmentPresenter> implements IBookRFragmentView {
-    TextView layoutRecommend;
-    TextView layoutMan;
-    TextView layoutWomen;
-    ViewPager viewPager;
-    List<BaseFragment> sourceList;
+public class BookRFragment extends BaseFragment<IBookRFragmentPresenter> implements IBookRFragmentView, View.OnClickListener {
+    public TextView layoutRecommend;
+    public TextView layoutMan;
+    public TextView layoutWomen;
+    private ViewPager viewPager;
+    private List<BaseFragment> sourceList;
+    ViewPagerIndicator viewPagerIndicator;
     /**
      * 推荐fragment
      */
-    BookRRecommendFImpl recommendFagment;
+    private BookRRecommendFImpl recommendFagment;
 
     /**
      * manFragment 男士专区
      */
-    BookRManFImpl manFragment;
+    private BookRManFImpl manFragment;
 
     /**
      * womanFragmen 女士专区
      */
-    BookRWomanFImpl womanFragmen;
+    private BookRWomanFImpl womanFragmen;
+
+    private static final int RECOMMENDFRAGMENT = 0;
+    private static final int MANFRAGMENT = 1;
+    private static final int WOMANFRAGMENT = 2;
 
     @Override
     protected void initData() {
@@ -51,9 +58,14 @@ public class BookRFragment extends BaseFragment<IBookRFragmentPresenter> impleme
     protected void bindView() {
         super.bindView();
         layoutRecommend = view.findViewById(R.id.mp_bookr_layout_recommend);
+        layoutRecommend.setOnClickListener(this);
         layoutMan = view.findViewById(R.id.mp_bookr_layout_men);
+        layoutMan.setOnClickListener(this);
         layoutWomen = view.findViewById(R.id.mp_bookr_layout_women);
+        layoutWomen.setOnClickListener(this);
         viewPager = view.findViewById(R.id.mp_bookr_viewpager);
+        viewPagerIndicator = view.findViewById(R.id.indicator_circle_line);
+
 
     }
 
@@ -64,6 +76,8 @@ public class BookRFragment extends BaseFragment<IBookRFragmentPresenter> impleme
         sourceList.add(manFragment);
         sourceList.add(womanFragmen);
         viewPager.setAdapter(new MybookViewPagerAdapter(getFragmentManager(), sourceList));
+        viewPager.addOnPageChangeListener(new MybookViewPageChangeListener());
+        viewPagerIndicator.setViewPager(viewPager, sourceList.size());
     }
 
     @Override
@@ -76,4 +90,57 @@ public class BookRFragment extends BaseFragment<IBookRFragmentPresenter> impleme
         return inflater.inflate(R.layout.mp_book_r_layout, container, false);
     }
 
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.mp_bookr_layout_recommend:
+                viewPager.setCurrentItem(RECOMMENDFRAGMENT);
+                break;
+            case R.id.mp_bookr_layout_men:
+                viewPager.setCurrentItem(MANFRAGMENT);
+                break;
+            case R.id.mp_bookr_layout_women:
+                viewPager.setCurrentItem(WOMANFRAGMENT);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private class MybookViewPageChangeListener implements ViewPager.OnPageChangeListener {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            switch (position) {
+                case RECOMMENDFRAGMENT:
+                    setSelectPageTextSize(layoutRecommend);
+                    break;
+                case MANFRAGMENT:
+                    setSelectPageTextSize(layoutMan);
+                    break;
+                case WOMANFRAGMENT:
+                    setSelectPageTextSize(layoutWomen);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+
+        private void setSelectPageTextSize(TextView text) {
+            layoutRecommend.setTextSize(20);
+            layoutMan.setTextSize(20);
+            layoutWomen.setTextSize(20);
+            text.setTextSize(26);
+        }
+    }
 }
